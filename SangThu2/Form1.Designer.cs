7e013fed7030a1d002d3be376f5fbe63a3c494d1
Cuong <nguyenchicuong11022003@gmail.com>
2025-03-04 13:24:16 +0700

    a3

--- SangThu2/Form1.Designer.cs
+++ SangThu2/Form1.Designer.cs
@@ -38,54 +38,58 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(363, 58);
+            this.label1.Location = new System.Drawing.Point(408, 72);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.Size = new System.Drawing.Size(88, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
+            this.label1.Text = "Đăng nhập";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(468, 53);
+            this.txtUserName.Location = new System.Drawing.Point(526, 66);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(100, 22);
+            this.txtUserName.Size = new System.Drawing.Size(112, 26);
             this.txtUserName.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(363, 92);
+            this.label2.Location = new System.Drawing.Point(408, 115);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.Size = new System.Drawing.Size(77, 20);
             this.label2.TabIndex = 0;
             this.label2.Text = "Mật Khẩu";
             // 
             // txtmatkhau
             // 
-            this.txtmatkhau.Location = new System.Drawing.Point(468, 92);
+            this.txtmatkhau.Location = new System.Drawing.Point(526, 115);
+            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtmatkhau.Name = "txtmatkhau";
-            this.txtmatkhau.Size = new System.Drawing.Size(100, 22);
+            this.txtmatkhau.Size = new System.Drawing.Size(112, 26);
             this.txtmatkhau.TabIndex = 2;
             // 
             // btnlogin
             // 
-            this.btnlogin.Location = new System.Drawing.Point(468, 137);
+            this.btnlogin.Location = new System.Drawing.Point(526, 171);
+            this.btnlogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.Size = new System.Drawing.Size(84, 29);
             this.btnlogin.TabIndex = 3;
             this.btnlogin.Text = "Login";
             this.btnlogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.btnlogin);
             this.Controls.Add(this.txtmatkhau);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
